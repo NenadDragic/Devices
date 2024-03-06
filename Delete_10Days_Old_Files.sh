25858d6c8e15d198c6327ba16e8d8ce981691507
Nenad Ahlstrøm Dragic <nenad@dragic.com>
2024-03-06 13:42:37 +0100

    One file a day, not one every hour

--- Delete_10Days_Old_Files.sh
+++ Delete_10Days_Old_Files.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-#find /mnt/sda1/ -type f -mtime +10
-#find /mnt/sda1/ -type f -mtime +10  | xargs rm -f
 find /mnt/sda1/ -type f -mtime +10 | tee /root/File-Delete/$(date +%y%m%d_%H%M%S).txt | xargs rm -f
-
-
--- File-Count-FTP-Upload.sh
+++ File-Count-FTP-Upload.sh
@@ -1,7 +1,82 @@
 #!/bin/bash
 
-sh /root/Scripts/Count_Files_SD.sh  > /root/File-Count-SD/"$(date '+%Y%m%d_%H%M%S').txt"
-sh /root/Scripts/Count_Files_DashCam.sh > /root/File-Count-DashCam/"$(date '+%Y%m%d_%H%M%S').txt"
 
-sshpass -v -f /root/Adm/pw.txt rsync -avz --ignore-existing /root/File-Count-SD SFTP@nas.dragic.com:/volume1/Ftp/DashCam
-sshpass -v -f /root/Adm/pw.txt rsync -avz --ignore-existing /root/File-Count-DashCam SFTP@nas.dragic.com:/volume1/Ftp/DashCam
+#
+# SD Card
+#
+
+# Get today's date
+today=$(date +%Y-%m-%d)
+
+# Define the filename
+filename="/root/File-Count-SD/${today}.txt"
+
+# Check if the file already exists
+if [ ! -f "$filename" ]; then
+    # If the file doesn't exist, create it
+    echo "This is a new file created on ${today}" > "$filename"
+else
+    # If the file does exist, append to it
+    echo "Additional data added on $(date)" >> "$filename"
+fi
+
+# Run the shell script and get its output
+output=$(sh /root/Scripts/Count_Files_SD.sh)
+
+# Append the output of the shell script to the file
+echo "Output of the shell script: " >> "$filename"
+echo "$output" >> "$filename"
+
+#
+# Dashcam
+#
+
+# Define the filename
+filename="/root/File-Count-DashCam/${today}.txt"
+
+# Check if the file already exists
+if [ ! -f "$filename" ]; then
+    # If the file doesn't exist, create it
+    echo "This is a new file created on ${today}" > "$filename"
+else
+    # If the file does exist, append to it
+    echo "Additional data added on $(date)" >> "$filename"
+fi
+
+# Run the shell script and get its output
+output=$(sh /root/Scripts/Count_Files_DashCam.sh)
+
+# Append the output of the shell script to the file
+echo "Output of the shell script: " >> "$filename"
+echo "$output" >> "$filename"
+
+
+#
+# File Delete
+#
+
+# Define the filename
+filename="/root/File-Delete/${today}.txt"
+
+# Check if the file already exists
+if [ ! -f "$filename" ]; then
+    # If the file doesn't exist, create it
+    echo "This is a new file created on ${today}" > "$filename"
+else
+    # If the file does exist, append to it
+    echo "Additional data added on $(date)" >> "$filename"
+fi
+
+# Run the shell script and get its output
+output=$(sh /root/Scripts/Delete_10Days_Old_Files.sh)
+
+# Append the output of the shell script to the file
+echo "Output of the shell script: " >> "$filename"
+echo "$output" >> "$filename"
+
+#
+# FTP files
+#
+sshpass -v -f /root/Adm/pw.txt rsync -avz /root/File-Count-SD SFTP@nas.dragic.com:/volume1/Ftp/DashCam
+sshpass -v -f /root/Adm/pw.txt rsync -avz /root/File-Count-DashCam SFTP@nas.dragic.com:/volume1/Ftp/DashCam
+sshpass -v -f /root/Adm/pw.txt rsync -avz /root/File-Delete SFTP@nas.dragic.com:/volume1/Ftp/DashCam
