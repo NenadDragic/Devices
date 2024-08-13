b930ccbf8861dcc2f89b9b4208a794dd2257d92c
Nenad Ahlstrøm Dragic <nenad@dragic.com>
2024-08-13 20:02:40 +0200

    New files added

--- Backup.sh
+++ Backup.sh
@@ -8,6 +8,6 @@ mkfifo /tmp/pw_pipe
 cat ../Adm/pw_nas.txt > /tmp/pw_pipe & 
 sudo sshpass -f /tmp/pw_pipe sudo rsync -av ~ -e "ssh -l Debian_Backup" nas.dragic.com::NetBackup/BackupData/Muddi-E750/$(date +%Y-%m-%d)
 
-
+rm /tmp/pw_pipe
 #Exit
 exit
--- Install.sh
+++ Install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
  opkg install nano
- #rsync
+ openssh-sftp-server
+ sshpass
+ rsync
+ nmap
+
+
+scp *  root@192.168.8.1:/root/Scripts/Old
+
