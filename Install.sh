7a8bbf49c0bb3d84c686f7b406aa676dcdea24ea
Nenad Ahlstrøm Dragic <nenad@dragic.com>
2025-03-04 22:53:07 +0100

    New script

--- Install.sh
+++ Install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
- opkg install nano
- openssh-sftp-server
- sshpass
- rsync
- nmap
+opkg install nano git openssh-sftp-server sshpass rsync nmap
+opkg update
+opkg list-upgradable | cut -f 1 -d ' ' | xargs opkg upgrade
 
+#scp *  root@192.168.8.1:/root/Scripts/Old
 
-scp *  root@192.168.8.1:/root/Scripts/Old
-
+#scp -r * root@192.168.1.1:/root 
